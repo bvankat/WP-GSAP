7977b01496ba7fd8abb1a6d9196f68b6023e9d4b
Benjamin Vankat <bvankat@gmail.com>
2024-10-25 10:00:23 -0500

    fix enqueue gsap namespace

--- wp-gsap-plugin.php
+++ wp-gsap-plugin.php
@@ -19,7 +19,7 @@ if (!defined('ABSPATH')) {
 // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 function theme_gsap_script(){
 	// The core GSAP library
-	wp_enqueue_script( 'wp-gsap', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js', array(), false, true );
+	wp_enqueue_script( 'gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js', array(), false, true );
 	// ScrollTrigger - with gsap.js passed as a dependency
 	wp_enqueue_script( 'gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js', array('gsap-js'), false, true );
 	// Your animation code file - with gsap.js passed as a dependency
